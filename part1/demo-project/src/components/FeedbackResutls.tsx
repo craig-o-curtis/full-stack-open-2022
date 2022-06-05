d86d2c5075e8570b57fbb8dbbf3c1c40149001d4
Craig Curtis <craig.curtis@menandmice.com>
2022-06-05 19:06:00 +0300

    Complete part 1.d 1.7

--- part1/demo-project/src/components/FeedbackResutls.tsx
+++ part1/demo-project/src/components/FeedbackResutls.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import { FeedbackType } from "./Feedback.types";
 interface Stat {
   good: number;
   neutral: number;
@@ -8,21 +8,49 @@ interface Stat {
 }
 
 const StatItem = styled.li``;
-
+const StrongUpper = styled.strong`
+  text-transform: capitalize;
+`;
 interface FeedbackResultsProps {
   stats: Stat;
 }
 
 const FeedbackResults = ({ stats }: FeedbackResultsProps) => {
+  const total = Object.values(stats).reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+
+  const averageMap: Record<string, number> = {
+    good: 1,
+    neutral: 0,
+    bad: -1,
+  };
+
+  const average =
+    Object.entries(stats).reduce((acc, [key, val]) => {
+      return acc + averageMap[key] * val;
+    }, 0) / total || 0;
+
+  const positive = Number((stats.good / (total || 1)) * 100).toFixed(2);
+
   return (
     <div>
       <h2>Statistics</h2>
       <ul>
         {Object.entries(stats).map(([key, value]) => (
           <StatItem key={key}>
-            <strong>{key}:</strong> {value}
+            <StrongUpper>{key}:</StrongUpper> {value}
           </StatItem>
         ))}
+        <StatItem>
+          <StrongUpper>All:</StrongUpper>: {total}
+        </StatItem>
+        <StatItem>
+          <StrongUpper>Average:</StrongUpper>: {average}
+        </StatItem>
+        <StatItem>
+          <StrongUpper>Postiive:</StrongUpper>: {positive}%
+        </StatItem>
       </ul>
     </div>
   );
