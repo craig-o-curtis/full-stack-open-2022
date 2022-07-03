1cbc1955be2d209b4250789b009639b68e377a33
Craig Curtis <craigcurtis@Craig-MacBook-Pro.local>
2022-07-03 14:09:04 +0300

    Add new CoursePart2 component with inner copied components, add id for key iterator

--- parts1-2/demo-project/src/components/Course/index.ts
+++ parts1-2/demo-project/src/components/Course/index.ts
@@ -1,1 +1,4 @@
 export { default } from "./Course";
+export { default as CourseHeader } from "./CourseHeader";
+export { default as CourseParts } from "./CourseParts";
+export { default as CourseTotals } from "./CourseTotals";
--- parts1-2/demo-project/src/components/index.ts
+++ parts1-2/demo-project/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Anecdotes } from "./Anecdotes";
 export { default as Course } from "./Course";
 export { default as Feedback } from "./Feedback";
+export { default as CoursePart2 } from "./CoursePart2";
