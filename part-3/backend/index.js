1e801ca6030f05b652e2a935f84637d3eea145df
Craig Curtis <craigocurtis@gmail.com>
2022-07-12 13:44:17 +0300

    Add simple redirect

--- part-3/backend/index.js
+++ part-3/backend/index.js
@@ -26,6 +26,10 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/api", (request, response) => {
+  response.redirect("/api/notes");
+});
+
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
